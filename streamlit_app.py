a6f321357d49dab497d6856bbd4bdab4b9eae241
Pitabasa5 <160697512+Pitabasa5@users.noreply.github.com>
2024-02-21 23:16:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blue berry Ota Meal')
+streamlit.text('Hard Boiled Free Range Egg')
